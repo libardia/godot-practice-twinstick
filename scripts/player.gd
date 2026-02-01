776c18e674f409dc40cb7581b84d38e135bb63f2
Anthony Libardi <libardi42@gmail.com>
2026-01-31 23:54:16 -0500

    player

---
+++ scripts/player.gd
@@ -0,0 +1,48 @@
+class_name Player
+extends CharacterBody2D
+
+
+@export_category("Movement")
+@export var max_speed: float = 750
+@export var drag_coeff: float = 0.3
+@export var mass: float = 10
+
+@export_category("Rotation")
+@export var rotation_lerp_factor: float = 15
+
+@onready var camera: Camera2D = $Camera2D
+
+var move_force: float
+var target_angle: float
+
+
+func _ready() -> void:
+    move_force = max_speed * drag_coeff
+
+
+func _process(delta: float) -> void:
+    rotation = lerp_angle(rotation, target_angle, rotation_lerp_factor * delta)
+
+
+func _physics_process(_delta: float) -> void:
+    var move_dir = Input.get_vector(&"move left", &"move right", &"move up", &"move down")
+    var look_dir: Vector2
+
+    if Settings.mouse_aim and InputDetector.is_mkb():
+        look_dir = position.direction_to(get_global_mouse_position())
+    else:
+        look_dir = Input.get_vector(&"aim left", &"aim right", &"aim up", &"aim down")
+
+    if look_dir.is_zero_approx():
+        if not move_dir.is_zero_approx():
+            target_angle = move_dir.angle()
+    else:
+        target_angle = look_dir.angle()
+
+    # Movement
+    var forces = move_dir * move_force
+    # Drag
+    forces += -velocity * drag_coeff
+
+    velocity += forces / mass
+    move_and_slide()
