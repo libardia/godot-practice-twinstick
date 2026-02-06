71cbd4ec1eeff183cadacc35597feacaffed82f4
Anthony Libardi <libardi42@gmail.com>
2026-02-06 14:29:54 -0500

    remove line (clean commit for tracking changes)

--- scripts/bullet_manager.gd
+++ scripts/bullet_manager.gd
@@ -9,7 +9,6 @@ func fire(scene: PackedScene, init_xform: Transform2D, velocity: Vector2):
         bullet_container = Node2D.new()
         bullet_container.z_index = -1
         get_tree().current_scene.add_child(bullet_container)
-        get_tree().current_scene.move_child(bullet_container, 0)
     var bullet: Bullet = scene.instantiate()
     bullet.velocity = velocity
     bullet.global_transform = init_xform
